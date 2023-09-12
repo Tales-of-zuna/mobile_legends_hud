7ebb67b2069f52dec8a3655bd812bef367f0648b
Zuunbileg <zuunbileg.b@unitel.mn>
2023-09-12 15:57:29 +0800

    admin v1

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,20 +1,61 @@
 "use client";
-import { useEffect } from "react";
-import io from "socket.io-client";
-let socket;
-const Admin = () => {
-  const socketInit = async () => {
-    await fetch("/api/socket");
-    socket = io();
-  };
-  useEffect(() => {
-    socketInit();
-  }, []);
+import { Switch, select } from "@nextui-org/react";
+import { useState } from "react";
 
+const Admin = () => {
+  const commands = [{ command: "ban" }];
+  const [selected, setSelected] = useState(false);
+  const swtitching = (name, idx) => {
+    return (
+      <div className="col-span-1 space-y-2">
+        <p>
+          {idx + 1}.{name} {String(selected)}
+        </p>
+        <Switch
+          onChange={async () => {
+            if (selected) {
+              await fetch("/api/ban", {});
+            }
+            setSelected(!selected);
+          }}
+          color="success"
+          className="m-0"
+          size="lg"
+        />
+      </div>
+    );
+  };
   return (
-    <div className=" min-h-screen bg-slate-100 flex justify-center items-center">
-      <div className=" p-16 text-4xl font-bold text-slate-700 rounded-xl bg-white shadow-lg hover:shadow-xl hover:scale-105 transition-all transform duration-300">
-        Hello Admin!
+    <div className=" h-screen grid grid-cols-4 gap-4 text-slate-700 bg-slate-100  p-16 justify-center items-center">
+      <div className="p-8 col-span-3 xl font-bold  uppercase gap-4  grid grid-cols-8 w-full rounded-xl bg-white shadow-lg  transition-all transform duration-300">
+        {swtitching("banana", 1)}
+      </div>
+      <div className="col-span-1 font-semibold bg-white shadow-lg h-full rounded-xl space-y-4 p-8">
+        <div className="h-1/3 space-y-4 overflow-auto">
+          <p className="text-xl">Currently ON</p>
+          <div className=" flex text-white gap-2 capitalize text-sm flex-wrap">
+            <div className="rounded-full font-bold py-1 px-2 bg-green-500">
+              Ban phase
+            </div>
+            <div className="rounded-full font-bold py-1 px-2 bg-green-500">
+              Ban phase
+            </div>
+            <div className="rounded-full font-bold py-1 px-2 bg-green-500">
+              Ban phase
+            </div>
+          </div>
+        </div>
+        <div className="h-2/3 space-y-4">
+          <p className="text-xl">Commands</p>
+          <div className=" text-slate-400">
+            <p className="hover:text-slate-800 hover:translate-x-2 transition-all transform duration-300">
+              1.Ban phase (banning)
+            </p>
+            <p className="hover:text-slate-800 hover:translate-x-2 transition-all transform duration-300">
+              1.Ban phase (banning)
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
