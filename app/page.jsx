7b1c1d0343838f54bbd9790516515d09ea687f50
Zuunbileg <zuunbileg.b@unitel.mn>
2023-09-15 18:12:43 +0800

    pop up example

--- app/page.jsx
+++ app/page.jsx
@@ -1,92 +1,92 @@
 "use client";
+
 import DraftingOverlay from "@/components/screens/draftingOverlay";
 import InGameOverlay from "@/components/screens/inGameOverlay";
-import InGameStat from "@/components/screens/InGameStat";
+import InGameStat from "@/components/screens/inGameStat";
 import ItemBuild from "@/components/screens/itemBuild";
 import PlayerStatsFull from "@/components/screens/playerStatsFull";
 import PlayerStatsLower from "@/components/screens/playerStatsLower";
 import RealTimeVictoryDefeatRate from "@/components/screens/realTimeVictoryDefeatRate";
-import React, { useEffect, useState } from "react";
-import { Image } from "@nextui-org/react";
-import Modal from 'react-modal'
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  let socket;
-  const [data, setData] = useState([]);
-  const [type, setType] = useState();
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   async function fetchWithTimeout(resource, options = {}) {
-
-    await new Promise(r => setTimeout(r, 5000));
+    await new Promise((r) => setTimeout(r, 5000));
     const { timeout = 8000 } = options;
-    
+
     const controller = new AbortController();
     const id = setTimeout(() => controller.abort(), timeout);
-  
+
     const response = await fetch(resource, {
       ...options,
-      signal: controller.signal  
+      signal: controller.signal,
     });
     clearTimeout(id);
-  
+
     return response;
   }
-  
 
-    async function getBattleDataRecursive (payload) {
-
-      try{
-        const response = await fetchWithTimeout(
-          'http://esportsdata.mobilelegends.com:30260/battledata?authkey=6646f93ab8cf795f3f78a7ed73469cf7&battleid=696401121647981998&dataid=0'
-        ,{timeout: 2000});
-        const data = await response.json();
-        data.data.incre_event_list.map(item => {
-          if (item.event_type == "kill_hero" && item.extra_param && item.extra_param[0] == "first_blood"){
-            // const array = [];
-            // array.push(item);
-            // setData(item);
-            // setType("FirstBlood");
-            setIsOpen(true);
-            hideIsOpen(1000);
-          }
+  async function getBattleDataRecursive(payload) {
+    try {
+      const response = await fetchWithTimeout(
+        "http://esportsdata.mobilelegends.com:30260/battledata?authkey=6646f93ab8cf795f3f78a7ed73469cf7&battleid=696401121647981998&dataid=0",
+        { timeout: 2000 }
+      );
+      const data = await response.json();
+      data.data.incre_event_list.map((item) => {
+        if (
+          item.event_type == "kill_hero" &&
+          item.extra_param &&
+          item.extra_param[0] == "first_blood"
+        ) {
+          // const array = [];
+          // array.push(item);
+          // setData(item);
+          // setType("FirstBlood");
+          setIsOpen(true);
+          hideIsOpen(10000);
+        }
       });
 
-        // console.log(new Date());
-        // console.log(data);
-      } catch (error){
-        console.log(error.name);
-      } finally {
-        getBattleDataRecursive(payload);
-      }
-  };
+      // console.log(new Date());
+      // console.log(data);
+    } catch (error) {
+      console.log(error.name);
+    } finally {
+      getBattleDataRecursive(payload);
+    }
+  }
 
   const hideIsOpen = async (duration) => {
-    await new Promise(r => setTimeout(r, duration));
-   setIsOpen(false)
-
+    await new Promise((r) => setTimeout(r, duration));
+    setIsOpen(false);
   };
 
   useEffect(() => {
-    let payload = { authkey: '6646f93ab8cf795f3f78a7ed73469cf7', battleid: '165489743059134510',  dataid: 0};
+    let payload = {
+      authkey: "6646f93ab8cf795f3f78a7ed73469cf7",
+      battleid: "165489743059134510",
+      dataid: 0,
+    };
 
     getBattleDataRecursive(payload);
-    
   }, []);
 
   const customStyles = {
     overlay: {
-       backgroundColor: 'rgba(255, 255, 255, 0)'
+      backgroundColor: "rgba(255, 255, 255, 0)",
     },
     content: {
-       top: 'auto',
-       left: '50%',
-       right: 'auto',
-       bottom: '0%',
-       marginRight: '-50%',
-       transform: 'translate(-50%, -50%)'
-    }
- }
-
+      top: "auto",
+      left: "50%",
+      right: "auto",
+      bottom: "0%",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
 
   const displayComponents = (name) => {
     if (name === "draftingOverlay") {
@@ -114,16 +114,45 @@ const Home = () => {
   };
   return (
     <div className="h-screen bg-green-500">
-      <div className="h-full">{displayComponents(type)}</div>
+      {/* <div className="h-full">{displayComponents(type)}</div> */}
       {/* <div>{showPopup(name)}</div> */}
-      <Modal isOpen={isOpen} ariaHideApp={false} onRequestClose={() => setIsOpen(false)} style={customStyles}>
-      <Image
-         alt=""
-         fill="true"
-         className="object-contain h-44"
-         src={"/assets/tortoise.jpg"}
+      {/* <Modal
+        isOpen={isOpen}
+        ariaHideApp={false}
+        onRequestClose={() => setIsOpen(false)}
+        style={customStyles}
+      >
+        <Image
+          alt=""
+          fill="true"
+          className="object-contain h-44"
+          src={"/assets/tortoise.jpg"}
         />
-         </Modal>
+      </Modal> */}
+
+      <div
+        className={` absolute  overflow-hidden  bottom-0 w-full flex justify-center`}
+      >
+        <div
+          className={` ${
+            isOpen ? "translate-y-0 opacity-100" : "translate-y-full opacity-0"
+          } h-96 relative w-96 transition-all transform duration-popup ease-in-out`}
+        >
+          <Image
+            alt=""
+            fill
+            className="object-contain rounded-t-lg"
+            src={"/assets/tortoise.jpg"}
+          />
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .duration-popup {
+            transition-duration: 3000ms;
+          }
+        `}
+      </style>
     </div>
   );
 };
