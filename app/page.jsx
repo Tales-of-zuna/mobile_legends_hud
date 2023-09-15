35b27dbb28640ebb392ceae380174a9fd7ca2a03
baljaa <baljinnyam.bat@unitel.mn>
2023-09-15 15:54:34 +0800

    recursive call to mlbb api

--- app/page.jsx
+++ app/page.jsx
@@ -1,34 +1,92 @@
 "use client";
 import DraftingOverlay from "@/components/screens/draftingOverlay";
 import InGameOverlay from "@/components/screens/inGameOverlay";
+import InGameStat from "@/components/screens/InGameStat";
 import ItemBuild from "@/components/screens/itemBuild";
 import PlayerStatsFull from "@/components/screens/playerStatsFull";
 import PlayerStatsLower from "@/components/screens/playerStatsLower";
 import RealTimeVictoryDefeatRate from "@/components/screens/realTimeVictoryDefeatRate";
-import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
+import React, { useEffect, useState } from "react";
+import { Image } from "@nextui-org/react";
+import Modal from 'react-modal'
 
 const Home = () => {
   let socket;
   const [data, setData] = useState([]);
   const [type, setType] = useState();
-  const socketInit = async () => {
-    socket = io("http://10.22.224.217:8080");
-    socket.on("connect", () => {
-      console.log("Successfully connected");
+  const [isOpen, setIsOpen] = useState(false)
+  async function fetchWithTimeout(resource, options = {}) {
+
+    await new Promise(r => setTimeout(r, 5000));
+    const { timeout = 8000 } = options;
+    
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), timeout);
+  
+    const response = await fetch(resource, {
+      ...options,
+      signal: controller.signal  
     });
-    socket.on("drafting overlay", (data) => {
-      setData(data);
-      setType("draftingOverlay");
-    });
-    socket.on("player stats lower", (data) => {
-      setData(data);
-      setType("playerStatsLower");
-    });
+    clearTimeout(id);
+  
+    return response;
+  }
+  
+
+    async function getBattleDataRecursive (payload) {
+
+      try{
+        const response = await fetchWithTimeout(
+          'http://esportsdata.mobilelegends.com:30260/battledata?authkey=6646f93ab8cf795f3f78a7ed73469cf7&battleid=696401121647981998&dataid=0'
+        ,{timeout: 2000});
+        const data = await response.json();
+        data.data.incre_event_list.map(item => {
+          if (item.event_type == "kill_hero" && item.extra_param && item.extra_param[0] == "first_blood"){
+            // const array = [];
+            // array.push(item);
+            // setData(item);
+            // setType("FirstBlood");
+            setIsOpen(true);
+            hideIsOpen(1000);
+          }
+      });
+
+        // console.log(new Date());
+        // console.log(data);
+      } catch (error){
+        console.log(error.name);
+      } finally {
+        getBattleDataRecursive(payload);
+      }
   };
+
+  const hideIsOpen = async (duration) => {
+    await new Promise(r => setTimeout(r, duration));
+   setIsOpen(false)
+
+  };
+
   useEffect(() => {
-    socketInit();
+    let payload = { authkey: '6646f93ab8cf795f3f78a7ed73469cf7', battleid: '165489743059134510',  dataid: 0};
+
+    getBattleDataRecursive(payload);
+    
   }, []);
+
+  const customStyles = {
+    overlay: {
+       backgroundColor: 'rgba(255, 255, 255, 0)'
+    },
+    content: {
+       top: 'auto',
+       left: '50%',
+       right: 'auto',
+       bottom: '0%',
+       marginRight: '-50%',
+       transform: 'translate(-50%, -50%)'
+    }
+ }
+
 
   const displayComponents = (name) => {
     if (name === "draftingOverlay") {
@@ -43,6 +101,8 @@ const Home = () => {
       return <PlayerStatsFull data={data.data} />;
     } else if (name === "PlayerStatsLower") {
       return <PlayerStatsLower data={data.data} />;
+    } else if (name === "FirstBlood") {
+      return <InGameStat data={data} />;
     }
   };
   const showPopup = (name) => {
@@ -56,6 +116,14 @@ const Home = () => {
     <div className="h-screen bg-green-500">
       <div className="h-full">{displayComponents(type)}</div>
       {/* <div>{showPopup(name)}</div> */}
+      <Modal isOpen={isOpen} ariaHideApp={false} onRequestClose={() => setIsOpen(false)} style={customStyles}>
+      <Image
+         alt=""
+         fill="true"
+         className="object-contain h-44"
+         src={"/assets/tortoise.jpg"}
+        />
+         </Modal>
     </div>
   );
 };
---
+++ components/screens/inGameStat.jsx
@@ -0,0 +1,13 @@
+const InGameStat = (props) => {
+    console.log("IN GAME STAT: " + JSON.stringify(props.data))
+    return <div className="h-screen bg-yellow-500">
+      <Image
+         alt=""
+         fill
+         className="object-contain h-44"
+         src={"/assets/tortoise.png"}
+        />
+    </div>;
+  };
+  
+  export default InGameStat;
