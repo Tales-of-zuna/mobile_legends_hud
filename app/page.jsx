c8f6bdf70e3d063cb884999992a1b0c630511883
zuna <tales.of.zuna@gmail.com>
2023-09-25 09:49:25 +0800

    images

--- app/page.jsx
+++ app/page.jsx
@@ -18,258 +18,258 @@ import PromoCodeComponent from "@/components/popups/promoCodeComponent";
 import React, { useEffect, useState } from "react";
 
 const Home = () => {
-    const [data, setData] = useState([]);
-    const [type, setType] = useState("");
-    const [popUpType, setPopUpType] = useState("");
-    const [commandType, setCommandType] = useState("");
-    const [turtleState, setTurtleState] = useState(false);
-    const [lordState, setLordState] = useState(false);
-    const [battleId, setBattleId] = useState();
-    const [teamScore, setTeamScore] = useState();
-    const [banpickState, setBanpickState] = useState(false);
-    const [playState, setPlayState] = useState(false);
-    const [promoCode, setPromoCode] = useState("");
-    const [popUpState, setPopUpState] = useState(false);
-    const bc = new BroadcastChannel("admin");
+  const [data, setData] = useState([]);
+  const [type, setType] = useState("");
+  const [popUpType, setPopUpType] = useState("");
+  const [commandType, setCommandType] = useState("");
+  const [turtleState, setTurtleState] = useState(false);
+  const [lordState, setLordState] = useState(false);
+  const [battleId, setBattleId] = useState();
+  const [teamScore, setTeamScore] = useState();
+  const [banpickState, setBanpickState] = useState(false);
+  const [playState, setPlayState] = useState(false);
+  const [promoCode, setPromoCode] = useState("");
+  const [popUpState, setPopUpState] = useState(false);
+  const bc = new BroadcastChannel("admin");
 
-    const turtleTimer = () => {
-        setTurtleState(true);
+  const turtleTimer = () => {
+    setTurtleState(true);
 
-        setTimeout(() => {
-            setTurtleState(false);
-        }, 5000);
-    };
+    setTimeout(() => {
+      setTurtleState(false);
+    }, 5000);
+  };
 
-    const lordTimer = () => {
-        setLordState(true);
+  const lordTimer = () => {
+    setLordState(true);
 
-        setTimeout(() => {
-            setLordState(false);
-        }, 5000);
-    };
+    setTimeout(() => {
+      setLordState(false);
+    }, 5000);
+  };
 
-    const popUpTimer = () => {
-        setPopUpState(true);
+  const popUpTimer = () => {
+    setPopUpState(true);
 
-        setTimeout(() => {
-            setPopUpState(false);
-            setPopUpType("");
-        }, 3000);
-    };
+    setTimeout(() => {
+      setPopUpState(false);
+      setPopUpType("");
+    }, 3000);
+  };
 
-    async function fetchWithTimeout(resource, options = {}) {
-        await new Promise((r) => setTimeout(r, 300));
-        const { timeout = 8000 } = options;
+  async function fetchWithTimeout(resource, options = {}) {
+    await new Promise((r) => setTimeout(r, 300));
+    const { timeout = 8000 } = options;
 
-        const controller = new AbortController();
-        const id = setTimeout(() => controller.abort(), timeout);
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), timeout);
 
-        const response = await fetch(resource, {
-            ...options,
-            signal: controller.signal,
+    const response = await fetch(resource, {
+      ...options,
+      signal: controller.signal,
+    });
+    clearTimeout(id);
+
+    return response;
+  }
+
+  useEffect(() => {}, []);
+
+  //on Message
+  bc.onmessage = (event) => {
+    if (event.data.type == "draftingOverlay") {
+      setData([]);
+      setType("");
+      setPopUpType("");
+      setCommandType("");
+      setTurtleState(false);
+      setLordState(false);
+      setBattleId(null);
+      setTeamScore(null);
+      setBanpickState(false);
+      setPlayState(false);
+      console.log("data fom admin" + event.data.data.battleId);
+      setTeamScore(event.data.data);
+      let payload = {
+        battleId:
+          // "608204114254271443",
+          event.data.data.battleId,
+        dataid: 0,
+      };
+      getBattleDataRecursive(payload);
+      // setType(event.data.type);
+    }
+    if (event.data.type == "state-update-screen") {
+      console.log("event from broadcast :", event);
+      setType(event.data.data);
+    } else if (event.data.type == "state-false-screen") {
+      setType(null);
+    }
+
+    if (event.data.type == "state-update-popup") {
+      setPopUpType(event.data.data);
+      setPromoCode(event.data.code);
+    } else if (event.data.type == "state-false-popup") {
+      setPopUpType(null);
+    }
+  };
+  async function getBattleDataRecursive(payload) {
+    let data;
+    console.log("id" + payload?.battleId);
+    try {
+      const response = await fetchWithTimeout(
+        "http://esportsdata.mobilelegends.com:30260/battledata?authkey=6646f93ab8cf795f3f78a7ed73469cf7&battleid=" +
+          payload?.battleId +
+          "&dataid=" +
+          payload?.dataid,
+        { timeout: 2000 }
+      );
+
+      data = await response.json();
+
+      // setData(data);
+      console.log(data.data.state);
+
+      if (data.data.state === "pick" || data.data.state === "ban") {
+        setData(data);
+        if (!banpickState) {
+          setType("draftingOverlay");
+        }
+        setBanpickState(true);
+      }
+
+      if (data.data.state === "play") {
+        setData(data);
+        if (!playState) {
+          setType("inGameOverlay");
+        }
+        setPlayState(true);
+      }
+
+      if (data.data.state === "pause") {
+      }
+
+      // if (data.data.state == "end") {
+      //     setType("");
+      //     setPopUpType("");
+      // }
+
+      // if (data.data.state == "adjust" || data.data.state == "loading") {
+      //     setType("");
+      //     setPopUpType("");
+      // }
+
+      // if (data.data.state == "end") {
+      //     // setData(data);
+      //     setType(null);
+      //     setPopUpType(null);
+      // }
+
+      // if (data.data.tortoise_left_time == 1 || data.data.tortoise_left_time == 2) {
+      //     if (!turtleState) {
+      //         console.log("turtle");
+      //         setPopUpType("turtle cam");
+      //         turtleTimer();
+      //     }
+      // }
+
+      // if (data.data.lord_left_time == 1 || data.data.lord_left_time == 2) {
+      //     if (!lordState) {
+      //         console.log("lord turluuu");
+      //         setPopUpType("lord cam");
+      //         lordTimer();
+      //     }
+      // }
+
+      if (data.data.incre_event_list != null) {
+        data.data.incre_event_list.map((item) => {
+          console.log(item);
+          if (
+            item.event_type == "kill_hero" &&
+            item.extra_param != undefined
+            // item.extra_param[0] == "first_blood"
+          ) {
+            console.log("EXTRA PARAM :", item.extra_param[0]);
+            if (!popUpState) {
+              setPopUpType(item.extra_param[0]);
+              popUpTimer();
+            }
+          }
         });
-        clearTimeout(id);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      console.log("state in finally: " + data.data.state);
+      // console.log("dataid: " + data.dataid);
 
-        return response;
+      if (data.data.state != "end") {
+        let payloadInitial = {
+          dataid: data.dataid,
+          battleId: payload.battleId,
+        };
+        getBattleDataRecursive(payloadInitial);
+      }
+    }
+  }
+
+  const displayComponents = (name) => {
+    if (name === "draftingOverlay") {
+      return <DraftingOverlay data={data.data} />;
+    } else if (name === "inGameOverlay") {
+      return <InGameOverlay teamScore={teamScore} data={data.data} />;
     }
 
-    useEffect(() => {}, []);
+    // else if (name === "RealTimeVictoryDefeatRate") {
+    //   return <RealTimeVictoryDefeatRate data={data.data} />;
+    // }
+    // else if (name === "ItemBuild") {
+    //     return <ItemBuild data={data.data} />;
+    // } else if (name === "PlayerStatsFull") {
+    //     return <PlayerStatsFull data={data.data} />;
+    // } else if (name === "PlayerStatsLower") {
+    //     return <PlayerStatsLower data={data.data} />;
+    // } else if (name === "FirstBlood") {
+    //     return <InGameStat data={data} />;
+    // }
+  };
 
-    //on Message
-    bc.onmessage = (event) => {
-        if (event.data.type == "draftingOverlay") {
-            setData([]);
-            setType("");
-            setPopUpType("");
-            setCommandType("");
-            setTurtleState(false);
-            setLordState(false);
-            setBattleId(null);
-            setTeamScore(null);
-            setBanpickState(false);
-            setPlayState(false);
-            console.log("data fom admin" + event.data.data.battleId);
-            setTeamScore(event.data.data);
-            let payload = {
-                battleId:
-                    // "608204114254271443",
-                    event.data.data.battleId,
-                dataid: 0,
-            };
-            getBattleDataRecursive(payload);
-            // setType(event.data.type);
-        }
-        if (event.data.type == "state-update-screen") {
-            console.log("event from broadcast :", event);
-            setType(event.data.data);
-        } else if (event.data.type == "state-false-screen") {
-            setType(null);
-        }
-
-        if (event.data.type == "state-update-popup") {
-            setPopUpType(event.data.data);
-            setPromoCode(event.data.code);
-        } else if (event.data.type == "state-false-popup") {
-            setPopUpType(null);
-        }
-    };
-    async function getBattleDataRecursive(payload) {
-        let data;
-        console.log("id" + payload?.battleId);
-        try {
-            const response = await fetchWithTimeout(
-                "http://esportsdata.mobilelegends.com:30260/battledata?authkey=6646f93ab8cf795f3f78a7ed73469cf7&battleid=" +
-                    payload?.battleId +
-                    "&dataid=" +
-                    payload?.dataid,
-                { timeout: 2000 }
-            );
-
-            data = await response.json();
-
-            // setData(data);
-            console.log(data.data.state);
-
-            if (data.data.state === "pick" || data.data.state === "ban") {
-                setData(data);
-                if (!banpickState) {
-                    setType("draftingOverlay");
-                }
-                setBanpickState(true);
-            }
-
-            if (data.data.state === "play") {
-                setData(data);
-                if (!playState) {
-                    setType("inGameOverlay");
-                }
-                setPlayState(true);
-            }
-
-            if (data.data.state === "pause") {
-            }
-
-            // if (data.data.state == "end") {
-            //     setType("");
-            //     setPopUpType("");
-            // }
-
-            // if (data.data.state == "adjust" || data.data.state == "loading") {
-            //     setType("");
-            //     setPopUpType("");
-            // }
-
-            // if (data.data.state == "end") {
-            //     // setData(data);
-            //     setType(null);
-            //     setPopUpType(null);
-            // }
-
-            // if (data.data.tortoise_left_time == 1 || data.data.tortoise_left_time == 2) {
-            //     if (!turtleState) {
-            //         console.log("turtle");
-            //         setPopUpType("turtle cam");
-            //         turtleTimer();
-            //     }
-            // }
-
-            // if (data.data.lord_left_time == 1 || data.data.lord_left_time == 2) {
-            //     if (!lordState) {
-            //         console.log("lord turluuu");
-            //         setPopUpType("lord cam");
-            //         lordTimer();
-            //     }
-            // }
-
-            if (data.data.incre_event_list != null) {
-                data.data.incre_event_list.map((item) => {
-                    console.log(item);
-                    if (
-                        item.event_type == "kill_hero" &&
-                        item.extra_param != undefined
-                        // item.extra_param[0] == "first_blood"
-                    ) {
-                        console.log("EXTRA PARAM :", item.extra_param[0]);
-                        if (!popUpState) {
-                            setPopUpType(item.extra_param[0]);
-                            popUpTimer();
-                        }
-                    }
-                });
-            }
-        } catch (error) {
-            console.log(error);
-        } finally {
-            console.log("state in finally: " + data.data.state);
-            // console.log("dataid: " + data.dataid);
-
-            if (data.data.state != "end") {
-                let payloadInitial = {
-                    dataid: data.dataid,
-                    battleId: payload.battleId,
-                };
-                getBattleDataRecursive(payloadInitial);
-            }
-        }
+  const showPopup = (popUptype) => {
+    // if (popUptype == "individual player stats") {
+    //     return <IndiPlayerStats data={data.data} />;
+    // } else if (popUptype == "in game stats head to head") {
+    //     return <HeadToHeadComponent data={data.data} />;
+    // } else if (popUptype == "in game stat") {
+    //     return <InGameStat data={data.data} />;
+    // } else if (popUptype == "turtle cam") {
+    //     return <TurtleCam data={data.data} />;
+    // } else if (popUptype == "lord cam") {
+    //     return <LordCam data={data.data} />;
+    // } else if (popUptype == "real time victory defeat rate") {
+    //     return <RealTimeVictoryDefeatRate data={data.data} />;
+    // } else if (popUptype == "team gold difference") {
+    //     return <TeamGoldDifference data={data.data} />;
+    if (
+      popUptype == "first_blood" ||
+      popUptype == "double_kill" ||
+      popUptype == "triple_kill" ||
+      popUptype == "quadra_kill" ||
+      popUptype == "penta_kill"
+    ) {
+      return <KillEventComp data={popUptype} />;
+    } else if (popUpType == "promo code") {
+      return <PromoCodeComponent promoCode={promoCode} />;
+      // <KillEventComp data={"first_blood"} />;
     }
-
-    const displayComponents = (name) => {
-        if (name === "draftingOverlay") {
-            return <DraftingOverlay data={data.data} />;
-        } else if (name === "inGameOverlay") {
-            return <InGameOverlay teamScore={teamScore} data={data.data} />;
-        }
-
-        // else if (name === "RealTimeVictoryDefeatRate") {
-        //   return <RealTimeVictoryDefeatRate data={data.data} />;
-        // }
-        // else if (name === "ItemBuild") {
-        //     return <ItemBuild data={data.data} />;
-        // } else if (name === "PlayerStatsFull") {
-        //     return <PlayerStatsFull data={data.data} />;
-        // } else if (name === "PlayerStatsLower") {
-        //     return <PlayerStatsLower data={data.data} />;
-        // } else if (name === "FirstBlood") {
-        //     return <InGameStat data={data} />;
-        // }
-    };
-
-    const showPopup = (popUptype) => {
-        // if (popUptype == "individual player stats") {
-        //     return <IndiPlayerStats data={data.data} />;
-        // } else if (popUptype == "in game stats head to head") {
-        //     return <HeadToHeadComponent data={data.data} />;
-        // } else if (popUptype == "in game stat") {
-        //     return <InGameStat data={data.data} />;
-        // } else if (popUptype == "turtle cam") {
-        //     return <TurtleCam data={data.data} />;
-        // } else if (popUptype == "lord cam") {
-        //     return <LordCam data={data.data} />;
-        // } else if (popUptype == "real time victory defeat rate") {
-        //     return <RealTimeVictoryDefeatRate data={data.data} />;
-        // } else if (popUptype == "team gold difference") {
-        //     return <TeamGoldDifference data={data.data} />;
-        if (
-            popUptype == "first_blood" ||
-            popUptype == "double_kill" ||
-            popUptype == "triple_kill" ||
-            popUptype == "quadra_kill" ||
-            popUptype == "penta_kill"
-        ) {
-            return <KillEventComp data={popUptype} />;
-        } else if (popUpType == "promo code") {
-            return <PromoCodeComponent promoCode={promoCode} />;
-            // <KillEventComp data={"first_blood"} />;
-        }
-    };
-    return (
-        <div className="h-screen bg-green-500">
-            <div className="h-full">
-                {displayComponents(type)}
-                {showPopup(popUpType)}
-            </div>
-        </div>
-    );
+  };
+  return (
+    <div className="h-screen bg-green-500">
+      <div className="h-full">
+        {displayComponents(type)}
+        {showPopup(popUpType)}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- components/popups/promoCodeComponent.jsx
+++ components/popups/promoCodeComponent.jsx
@@ -1,23 +1,29 @@
 import Image from "next/image";
 const promoCodeComponent = (props) => {
-    return (
-        <div className="h-screen top-0 left-0 w-screen absolute z-10">
-            <div className="absolute w-full bottom-0 h-1/3 flex justify-center overflow-y-hidden">
-                <div
-                    className={`${"translate-y-0 opacity-100"} relative h-96 ease-in-out transition-all transform duration-3000 w-1/3`}>
-                    <Image className="object-contain " fill alt="" src={"/assets/promo_code.png"} />
-                    <div className="absolute z-10 left-80 top-44 text-slate-100 text-2xl font-semibold font-sans">
-                        {props.promoCode}
-                    </div>
-                </div>
-                <style jsx>{`
-                    .duration-3000 {
-                        transition-duration: 100ms;
-                    }
-                `}</style>
-            </div>
+  return (
+    <div className="h-screen top-0 left-0 w-screen absolute z-10">
+      <div className="absolute w-full bottom-0 h-1/3 flex justify-center overflow-y-hidden">
+        <div
+          className={`${"translate-y-0 opacity-100"} relative h-96 ease-in-out transition-all transform duration-3000 w-1/3`}
+        >
+          <Image
+            className="object-contain "
+            fill
+            alt=""
+            src={"/assets/promo_code.png"}
+          />
+          <div className="absolute z-10 left-80 top-44 text-slate-100 text-2xl font-semibold font-sans">
+            {props.promoCode}
+          </div>
         </div>
-    );
+        <style jsx>{`
+          .duration-3000 {
+            transition-duration: 100ms;
+          }
+        `}</style>
+      </div>
+    </div>
+  );
 };
 
 export default promoCodeComponent;
